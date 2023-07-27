4d9df765a650a1be2136a6b0cc475c3decbf7be2
Yevgeniy343 <rt4387@yahoo.com>
2023-07-27 14:32:10 +0300

    remind pass

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,11 @@
 import User from "../models/Users.js";
 import { StatusCodes } from "http-status-codes";
-
 import {
   BadRequestError,
   UnAuthenticatedError,
 } from "../errors/index-errors.js";
+import nodemailer from "nodemailer";
+import { generate } from "random-words";
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -44,4 +45,53 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ user, token });
 };
 
-export { signup, login };
+const remind = async (req, res) => {
+  const { remind_email } = req.body;
+  console.log(remind_email);
+  if (!remind_email) {
+    throw new BadRequestError("Укажите Email");
+  }
+  const word = generate({ minLength: 4, maxLength: 6 });
+  const number = Math.floor(Math.random() * 8999 + 1000);
+  const new_pass = word + number;
+  console.log(word);
+  console.log(number);
+  console.log(new_pass);
+  let user;
+  user = await User.findOne({ email: remind_email });
+  if (!user) {
+    throw new UnAuthenticatedError(
+      "Мы не нашли Вас в зарегистрированных пользователях. Проверьте правильность указанной почты "
+    );
+  }
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "kronujin@gmail.com",
+      pass: process.env.APP,
+    },
+  });
+
+  var mailOptions = {
+    from: "kronujin@gmail.com",
+    to: `${remind_email}`,
+    subject: "Новый пароль",
+    text: "",
+    html: `<h1 style="color:#a6a28e">${new_pass}</h1>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(StatusCodes.OK).json(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(StatusCodes.OK).json("Email sent: " + info.response);
+    }
+  });
+};
+
+export { signup, login, remind };
+
+// fjbgswbugzieiaho
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { signup, login } from "../controllers/authControllers.js";
+import { signup, login, remind } from "../controllers/authControllers.js";
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
+router.route("/remind").post(remind);
 
 export default router;
